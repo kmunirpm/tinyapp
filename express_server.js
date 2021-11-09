f8812c12450a03ba0841b67b2950c8e660248bc7
kmunirpm <km092009@gmail.com>
2021-11-09 16:18:29 +0000

    Server file more changes 11

--- express_server.js
+++ express_server.js
@@ -38,8 +38,14 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  //const shorturl= generateRandomString();
+  urlDatabase[generateRandomString()] = req.body.longURL;
   res.send(`Ok ${generateRandomString()}`);         // Respond with 'Ok' (we will replace this)
 });
 
