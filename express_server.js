37a3ffda5f120687801845fc26457ba680a5ad58
kmunirpm <km092009@gmail.com>
2021-11-26 01:51:40 +0000

    formatting with prettier applied on code.

--- express_server.js
+++ express_server.js
@@ -1,43 +1,50 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 const bodyParser = require("body-parser");
-const bcrypt = require('bcryptjs');
-const cookieSession = require('cookie-session');
-const {users, urlDatabase} = require('./helpers/userDB');
-const generateUserHelpers = require('./helpers/userHelper');
-const {urlsForUser, validateCredentialsFields, validateAction, generateRandomString, userLoggedIn} = generateUserHelpers(users, urlDatabase);
+const bcrypt = require("bcryptjs");
+const cookieSession = require("cookie-session");
+const { users, urlDatabase } = require("./helpers/userDB");
+const generateUserHelpers = require("./helpers/userHelper");
+const {
+  urlsForUser,
+  validateCredentialsFields,
+  validateAction,
+  generateRandomString,
+  userLoggedIn,
+} = generateUserHelpers(users, urlDatabase);
 
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}))
-
-
-app.use(bodyParser.urlencoded({extended: false})); //handles just primitive data types
-app.use(bodyParser.urlencoded({extended: true})); // handles every data type
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-
-
 app.get("/", (req, res) => {
-  return res.redirect('/urls');
+  return res.redirect("/urls");
 });
 
-//Lists all urls when the use 1st logins
+//Lists all urls if the user is logged in
 app.get("/urls", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
-  const templateVars = { urls: urlsForUser(req.session.user_id),  userId: users[req.session.user_id].email};
+  const userId = req.session.user_id;
+  if (!userLoggedIn(userId)) return res.redirect("/login");
+
+  const templateVars = {
+    urls: urlsForUser(userId),
+    userId: users[userId].email,
+  };
   res.render("urls_index", templateVars);
 });
 
 //Loads the form for adding new urls
 app.get("/urls/new", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
-  const templateVars = { userId: users[req.session.user_id].email};
+  if (!userLoggedIn(req.session.user_id)) return res.redirect("/login");
+
+  const templateVars = { userId: users[req.session.user_id].email };
   res.render("urls_new", templateVars);
 });
 
@@ -49,67 +56,79 @@ app.get("/u/:shortURL", (req, res) => {
 
 //Submits new URL request
 app.post("/urls", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
-  const randomid = generateRandomString();
-  urlDatabase[randomid] = {longURL: req.body.longURL, userID: req.session.user_id};
-  res.redirect(`/urls/${randomid}`);
+  if (!userLoggedIn(req.session.user_id)) return res.redirect("/login");
+
+  const randomId = generateRandomString();
+  urlDatabase[randomId] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id,
+  };
+  res.redirect(`/urls/${randomId}`);
 });
 
 //View the page for selected shorturl
 app.get("/urls/:shortURL", (req, res) => {
-  const shrtUrl = req.params.shortURL;
+  const shortURL = req.params.shortURL;
   const UID = req.session.user_id;
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
-  const val = validateAction(shrtUrl, UID);
-  if (val[0] === false)
-    return res.status(val[1]).send(val[2]);
-  const templateVars = { shortURL: shrtUrl, longURL: urlDatabase[shrtUrl].longURL, userId: users[UID].email};
+  if (!userLoggedIn(req.session.user_id)) return res.redirect("/login");
+
+  const val = validateAction(shortURL, UID);
+  if (!(val[0])) return res.status(val[1]).send(val[2]);
+
+  const templateVars = {
+    shortURL,
+    longURL: urlDatabase[shortURL].longURL,
+    userId: users[UID].email,
+  };
   res.render("urls_show", templateVars);
 });
 
 //Loads the edit page for selected shorturl
 app.get("/urls/:shortURL/edit", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
+  const userId = req.session.user_id;
+  if (!userLoggedIn(userId)) return res.redirect("/login");
+
   const shortURL = req.params.shortURL;
-  const valUser = validateAction(shortURL,req.session.user_id)
-  if( valUser[0] === false)
-    return res.status(valUser[1]).send(valUser[2]);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, userId: users[req.session.user_id].email};
+  const valUser = validateAction(shortURL, userId);
+  if (!(valUser[0])) return res.status(valUser[1]).send(valUser[2]);
+
+  const templateVars = {
+    shortURL,
+    longURL: urlDatabase[shortURL].longURL,
+    userId: users[userId].email,
+  };
   res.render("urls_edit", templateVars);
 });
 
 //Submits the edit longurl request for selected shorturl
 app.post("/urls/:shortURL/edit", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
+  if (!userLoggedIn(req.session.user_id)) return res.redirect("/login");
+
   const longURL = req.body.longURL;
-  const valUser = validateAction(req.params.shortURL,req.session.user_id)
-  if( valUser[0] === false)
-    return res.status(valUser[1]).send(valUser[2]);
+  const valUser = validateAction(req.params.shortURL, req.session.user_id);
+  if (!(valUser[0])) return res.status(valUser[1]).send(valUser[2]);
+
   urlDatabase[req.params.shortURL].longURL = longURL;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 //Submits the delete request for selected shorturl
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    return res.redirect('/login');
+  if (!userLoggedIn(req.session.user_id)) return res.redirect("/login");
+
   const shortURL = req.params.shortURL;
-  const valUser = validateAction(shortURL,req.session.user_id)
-  if( valUser[0] === false)
-    return res.status(valUser[1]).send(valUser[2]);
-  delete urlDatabase[shortURL]
-  res.redirect('/urls');
+  const valUser = validateAction(shortURL, req.session.user_id);
+  if (!(valUser[0])) return res.status(valUser[1]).send(valUser[2]);
+
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
 //Loads the register form
 app.get("/register", (req, res) => {
-  if(!userLoggedIn(req.session.user_id))
-    res.render("urls_register");
-  res.redirect('/urls');
+  if (!userLoggedIn(req.session.user_id)) res.render("urls_register");
+
+  res.redirect("/urls");
 });
 
 //Submits the register form
@@ -117,30 +136,29 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password);
   const val = validateCredentialsFields(users, email, password, "register");
-  if (val[0] === false)
-    return res.status(val[1]).send(val[2]);
-  
+  if (!val[0]) return res.status(val[1]).send(val[2]);
+
   const id = generateRandomString();
   users[id] = {
-    id, 
+    id,
     email,
     password,
-  }
+  };
   req.session.user_id = id;
-  res.redirect('/urls')
+  res.redirect("/urls");
 });
 
 //Submits the login form
 app.post("/login", (req, res) => {
-  const login = req.body.id
+  const login = req.body.id;
   const email = req.body.email;
   const password = req.body.password;
   const val = validateCredentialsFields(users, email, password, "login");
-  if (val[0] === false)
-    return res.status(val[1]).send(val[2]);
-  
+  console.log(val)
+  if (!val[0] === false) return res.status(val[1]).send(val[2]);
+
   req.session.user_id = val.id;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 //Loads the login form
@@ -149,15 +167,15 @@ app.get("/login", (req, res) => {
 });
 
 //Submits the logout request
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 //Loads the 404 page for unhandled routes
 app.get("*", (req, res) => {
-  const templateVars = { greeting: '404! Page not found' };
-  res.status(404)
+  const templateVars = { greeting: "404! Page not found" };
+  res.status(404);
   res.render("404", templateVars);
 });
 
--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -1,50 +1,62 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const generateUserHelpers = (users, urlDatabase) => {
-
   //validates users actions
   const validateAction = (urlid, userId) => {
-    if(typeof userId === "undefined" || userId === "")
-      return [err, code, msg] = [false, 400, "Login required."];
+    if (typeof userId === "undefined" || userId === "")
+      return ([err, code, msg] = [false, 400, "Login required."]);
     if (typeof urlDatabase[urlid] === "undefined") {
-      return [err, code, msg] = [false, 403, `Sorry! ${urlid} short URL is an invalid URL.`];
+      return ([err, code, msg] = [
+        false,
+        403,
+        `Sorry! ${urlid} short URL is an invalid URL.`,
+      ]);
+    } else if (urlDatabase[urlid].userID !== userId) {
+      return ([err, code, msg] = [
+        false,
+        403,
+        `Sorry! ${urlid} short URL does not belong to you.`,
+      ]);
     }
-    else if (urlDatabase[urlid].userID !== userId) {
-      return [err, code, msg] = [false, 403, `Sorry! ${urlid} short URL does not belong to you.`];
-    }
-    return [] = [true];
-  }
+    return ([] = [true]);
+  };
 
   //valiadates user authentication and new user registration
-  const validateCredentialsFields = (users, email, password, functionality) => 
-  {
-    if (email === '' || password === '') {
-        return [err, code, msg] = [false, 400, 'E-Mail and Password cannot be blank!'];
+  const validateCredentialsFields = (users, email, password, functionality) => {
+    if (email === "" || password === "") {
+      return ([err, code, msg] = [
+        false,
+        400,
+        "E-Mail and Password cannot be blank!",
+      ]);
     }
-    
+
     const user = findUserByEmail(email);
     if (!user && functionality === "login") {
-      return [err, code, msg] = [false, 403, `No user with ${email} found`];
-    }
-    else if (user && functionality === "register") {
-      return [err, code, msg] = [false, 400, `${email} is already in use!`];
+      return ([err, code, msg] = [false, 403, `No user with ${email} found`]);
+    } else if (user && functionality === "register") {
+      return ([err, code, msg] = [false, 400, `${email} is already in use!`]);
     }
     if (functionality === "login") {
       if (!bcrypt.compareSync(password, user.password)) {
-        return [err, code, msg] = [false, 403, `Password does not match ${email}'s saved password`];
+        return ([err, code, msg] = [
+          false,
+          403,
+          `Password does not match ${email}'s saved password`,
+        ]);
       }
     }
 
     if (functionality === "register") {
-      return [] = [true]
+      return ([] = [true]);
     } else {
       return user;
     }
-
-  }
+  };
 
   const urlsForUser = (id) => {
-    let userDb = {}, url;
+    let userDb = {},
+      url;
     for (let urlId in urlDatabase) {
       url = urlDatabase[urlId];
       if (url.userID === id) {
@@ -52,19 +64,18 @@ const generateUserHelpers = (users, urlDatabase) => {
       }
     }
     return userDb;
-  }
+  };
 
   //generates random strings for new users & new urls
   const generateRandomString = function () {
-    charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var randomString = '';
-      for (var i = 0; i < 6; i++) {
-          var randomPoz = Math.floor(Math.random() * charSet.length);
-          randomString += charSet.substring(randomPoz,randomPoz+1);
-      }
-      return randomString;
-  }
-
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var randomString = "";
+    for (var i = 0; i < 6; i++) {
+      var randomPoz = Math.floor(Math.random() * charSet.length);
+      randomString += charSet.substring(randomPoz, randomPoz + 1);
+    }
+    return randomString;
+  };
 
   const findUserByEmail = (email) => {
     for (const userId in users) {
@@ -74,25 +85,35 @@ const generateUserHelpers = (users, urlDatabase) => {
       }
     }
     return null;
-  }
+  };
 
-  //to ensure that once server restarts all previous sessions become invalid 
+  //to ensure that once server restarts all previous sessions become invalid
   const findUserByID = (ID) => {
-    if(typeof ID !== "undefined" && typeof users[ID] !== "undefined")
-      return true
+    if (typeof ID !== "undefined" && typeof users[ID] !== "undefined")
+      return true;
     return false;
-  }
+  };
 
   //validates if user is logged in.
   const userLoggedIn = (session) => {
-    if(typeof session == "undefined" || session == "" || !findUserByID(session))
-        return false;
+    if (
+      typeof session == "undefined" ||
+      session == "" ||
+      !findUserByID(session)
+    )
+      return false;
     //if(validateAction(session))
     return true;
-  }
+  };
 
-  return {urlsForUser, findUserByEmail, validateCredentialsFields, validateAction, generateRandomString, userLoggedIn};
-}
-
+  return {
+    urlsForUser,
+    findUserByEmail,
+    validateCredentialsFields,
+    validateAction,
+    generateRandomString,
+    userLoggedIn,
+  };
+};
 
 module.exports = generateUserHelpers;
